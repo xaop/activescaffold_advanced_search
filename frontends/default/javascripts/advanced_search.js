6c7ddece46514bde854e7ee15d75de8b4580ba92
Peter Vanbroekhoven <peter@xaop.com>
2009-07-06 17:49:00 +0200

    * Added integer and boolean filters * Added negation to text filters * Added extra matching option to text filters * Matching text with equals on empty string also matches NULL (ActiveScaffold uses NULL instead of empty string) * Reset now clears the extra criteria such that the list is not emptied when using conditions that generally don't match when the search string is empty

--- frontends/default/javascripts/advanced_search.js
+++ frontends/default/javascripts/advanced_search.js
@@ -1,11 +1,45 @@
-function asAddCriterion(element) {
-  //$(element).up(".as_inputs");
+function asAddCriterion(element, type_map) {
   var fields = $("as_template").cloneNode(true);
   fields.id = null;
   fields.style.display = "block";
   element = $(element).up(".as_inputs");
   element.parentNode.insertBefore(fields, element);
+  var as_field = fields.down(".as_field");
+  as_field.observe("change", function (event) { asUpdateSearchType(as_field, type_map); });
+  asUpdateSearchType(as_field, type_map);
   new TextFieldWithExample(fields.down("input"), 'Search Term');
+}
+
+function asUpdateSearchType(element, type_map) {
+  var container = element.up(".as_criterion");
+  switch (type_map[element.getValue()]) {
+    case "integer" :
+      container.down(".as_negator").show();
+      container.down(".as_matcher").hide();
+      container.down(".as_search").hide();
+      container.down(".as_b_matcher").hide();
+      container.down(".as_i_matcher").show();
+      container.down(".as_i_search").show();
+      break;
+    case "text" :
+      container.down(".as_negator").show();
+      container.down(".as_matcher").show();
+      container.down(".as_search").show();
+      container.down(".as_b_matcher").hide();
+      container.down(".as_i_matcher").hide();
+      container.down(".as_i_search").hide();
+      break;
+    case "boolean" :
+      container.down(".as_negator").hide();
+      container.down(".as_matcher").hide();
+      container.down(".as_search").hide();
+      container.down(".as_b_matcher").show();
+      container.down(".as_i_matcher").hide();
+      container.down(".as_i_search").hide();
+      break;
+    default :
+      alert("oops");
+  }
 }
 
 function asRemoveCriterion(element) {
--- lib/advanced_finder.rb
+++ lib/advanced_finder.rb
@@ -2,7 +2,7 @@ module ActiveScaffold
 
   module AdvancedFinder
     
-    def self.create_conditions_for_columns(columns, fields, modes, values)
+    def self.create_conditions_for_columns(columns, fields, negators, modes, values, boolean_modes, integer_modes, integer_values)
       columns_hash = columns.inject({}) { |h, c| h[c.name.to_s] = c ; h }
       
       # if there aren't any columns, then just return a nil condition
@@ -10,25 +10,71 @@ module ActiveScaffold
 
       where_clauses = []
       tokens = []
-      fields.zip(modes).zip(values).each do |(f, m), v|
+      fields.zip(negators, modes, values, boolean_modes, integer_modes, integer_values).each do |f, n, m, v, bm, im, iv|
         if c = columns_hash[f]
-          search_sql = "LOWER(#{c.search_sql})"
-          v = (v || '').downcase
-          case m
-          when "equals"
-            where_clauses << "#{search_sql} = ?"
-            tokens << v
-          when "contains"
-            where_clauses << "#{search_sql} LIKE ?"
-            tokens << "%#{v}%"
-          when "starts"
-            where_clauses << "#{search_sql} LIKE ?"
-            tokens << "#{v}%"
-          when "ends"
-            where_clauses << "#{search_sql} LIKE ?"
-            tokens << "%#{v}"
+          case c.column.type
+          when :string
+            search_sql = c.search_sql
+            v = (v || '').downcase
+            case m
+            when "equals"
+              if v.empty?
+                where_clause = "(#{search_sql} LIKE ? ESCAPE '!' OR #{search_sql} IS NULL)"
+              else
+                where_clause = "#{search_sql} LIKE ? ESCAPE '!'"
+              end
+              tokens << v.gsub(/([_%!])/) { "!#{$1}" }
+            when "contains"
+              where_clause = "#{search_sql} LIKE ? ESCAPE '!'"
+              tokens << "%#{v.gsub(/([_%!])/) { "!#{$1}" }}%"
+            when "starts"
+              where_clause = "#{search_sql} LIKE ? ESCAPE '!'"
+              tokens << "#{v.gsub(/([_%!])/) { "!#{$1}" }}%"
+            when "ends"
+              where_clause = "#{search_sql} LIKE ? ESCAPE '!'"
+              tokens << "%#{v.gsub(/([_%!])/) { "!#{$1}" }}"
+            when "matches"
+              where_clause = "#{search_sql} LIKE ?"
+              tokens << v
+            else
+              raise "not yet #{m}"
+            end
+            if n == "not"
+              where_clauses << "NOT(#{where_clause})"
+            else
+              where_clauses << where_clause
+            end
+          when :integer
+            case im
+            when "equals"
+              where_clause = "#{c.search_sql} = #{iv.to_i}"
+            when "lt"
+              where_clause = "#{c.search_sql} < #{iv.to_i}"
+            when "gt"
+              where_clause = "#{c.search_sql} > #{iv.to_i}"
+            when "le"
+              where_clause = "#{c.search_sql} <= #{iv.to_i}"
+            when "ge"
+              where_clause = "#{c.search_sql} >= #{iv.to_i}"
+            else
+              raise "not yet #{im.inspect}"
+            end
+            if n == "not"
+              where_clauses << "NOT(#{where_clause})"
+            else
+              where_clauses << where_clause
+            end
+          when :boolean
+            case bm
+            when "true"
+              where_clauses << "#{c.search_sql} = 1"
+            when "false"
+              where_clauses << "#{c.search_sql} != 1"
+            else
+              raise "not yet #{bm.inspect}"
+            end
           else
-            raise "not yet #{m}"
+            raise "not yet #{c.column.type}"
           end
         end
       end
